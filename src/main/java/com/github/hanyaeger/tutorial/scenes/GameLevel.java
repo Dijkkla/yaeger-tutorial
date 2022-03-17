4ae22c1941163d8583c4484b85d8fc574bc6a0d9
Dijkkla <11039lodewijk@gmail.com>
2022-03-17 10:33:06 +0100

    completed step 8

--- src/main/java/com/github/hanyaeger/tutorial/scenes/GameLevel.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/GameLevel.java
@@ -3,10 +3,18 @@ package com.github.hanyaeger.tutorial.scenes;
 import com.github.hanyaeger.api.AnchorPoint;
 import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.scenes.DynamicScene;
+import com.github.hanyaeger.tutorial.Waterworld;
 import com.github.hanyaeger.tutorial.entities.Lad;
 import com.github.hanyaeger.tutorial.entities.Swordfish;
+import com.github.hanyaeger.tutorial.entities.text.HealthText;
 
 public class GameLevel extends DynamicScene {
+
+	private Waterworld waterworld;
+
+	public GameLevel(Waterworld waterworld){
+	    this.waterworld = waterworld;
+	}
 
 	@Override
 	public void setupScene() {
@@ -18,8 +26,11 @@ public class GameLevel extends DynamicScene {
 	public void setupEntities() {
 		var swordfish = new Swordfish(new Coordinate2D(getWidth() / 2, getHeight() / 2));
 		addEntity(swordfish);
+		
+		var healthText = new HealthText(new Coordinate2D(getWidth() / 4 * 3, getHeight() / 4));
+		addEntity(healthText);
 
-		var lad = new Lad(new Coordinate2D(getWidth() / 4, getHeight() / 4));
+		var lad = new Lad(new Coordinate2D(getWidth() / 4, getHeight() / 4), healthText, waterworld);
 		addEntity(lad);
 	}
 
--- src/main/java/com/github/hanyaeger/tutorial/scenes/GameOverScene.java
+++ src/main/java/com/github/hanyaeger/tutorial/scenes/GameOverScene.java
@@ -5,17 +5,19 @@ import com.github.hanyaeger.api.Coordinate2D;
 import com.github.hanyaeger.api.entities.impl.TextEntity;
 import com.github.hanyaeger.api.scenes.StaticScene;
 import com.github.hanyaeger.tutorial.Waterworld;
+import com.github.hanyaeger.tutorial.entities.buttons.QuitButton;
+import com.github.hanyaeger.tutorial.entities.buttons.ReplayButton;
 import com.github.hanyaeger.tutorial.entities.buttons.StartButton;
 
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 
-public class TitleScene extends StaticScene {
-	
+public class GameOverScene extends StaticScene {
+
 	private Waterworld waterworld;
 
-	public TitleScene(Waterworld waterworld){
+	public GameOverScene(Waterworld waterworld){
 	    this.waterworld = waterworld;
 	}
 
@@ -29,16 +31,20 @@ public class TitleScene extends StaticScene {
 	public void setupEntities() {
 		var waterworldText = new TextEntity(
 	        new Coordinate2D(getWidth() / 2, getHeight() / 2),
-	        "Waterworld"
+	        "Game Over"
 	    );
 	    waterworldText.setAnchorPoint(AnchorPoint.CENTER_CENTER);
 	    waterworldText.setFill(Color.DARKBLUE);
 	    waterworldText.setFont(Font.font("Roboto", FontWeight.SEMI_BOLD, 80));
 	    addEntity(waterworldText);
 	    
-	    var startButton = new StartButton(new Coordinate2D(getWidth() / 2, getHeight() / 3 * 2), waterworld);
-	    startButton.setAnchorPoint(AnchorPoint.CENTER_CENTER);
-	    addEntity(startButton);
+	    var replayButton = new ReplayButton(new Coordinate2D(getWidth() / 2, getHeight() / 5 * 3), waterworld);
+	    replayButton.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+	    addEntity(replayButton);
+	    
+	    var quitButton = new QuitButton(new Coordinate2D(getWidth() / 2, getHeight() / 5 * 4), waterworld);
+	    quitButton.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+	    addEntity(quitButton);
 	}
 
 }
